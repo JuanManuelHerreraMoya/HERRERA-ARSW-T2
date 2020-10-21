bc43b893cedad16cd698568bb310e15cb6486286
Juan Manul Herrera Moya <juan.herrera-m@mail.escuelaing.educ.co>
2020-10-21 12:10:08 -0500

    base

---
+++ src/main/java/edu/eci/arsw/OpenWather/OpenWatherApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.OpenWather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenWatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenWatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/arsw/OpenWather/OpenWatherApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.OpenWather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenWatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
